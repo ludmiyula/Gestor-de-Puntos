bdb12f9bb486e4b401a4829e8e816d9dbfd07d7d
Ludmila <ludmila.mignone@hotmail.com>
2024-09-17 13:54:32 -0300

    realizamos mejoras

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const calendar = document.querySelector(".calendar"),
-   date =document.querySelector(".date"),
+   date = document.querySelector(".date"),
    daysContainer = document.querySelector(".days"),
    prev = document.querySelector(".prev");
 (next = document.querySelector(".next"));
@@ -30,7 +30,7 @@ const months = [
 // Agregamos la funcion para dias
 function initCalendar() {  //para obtener los días del mes anterior y el mes actual todos los días y los días del mes siguiente
     const firstDay = new Date(year, month, 1);
-    const lastDay = new  Date(year, month +1, 0);
+    const lastDay = new  Date(year, month + 1, 0);
     const prevlastDay = new Date(year, month, 0);
     const prevDays = prevlastDay.getDate();
     const lastDate = lastDay.getDate();
@@ -46,7 +46,7 @@ function initCalendar() {  //para obtener los días del mes anterior y el mes ac
 
     // dia del mes anterior
     for(let x = days; x > 0; x--){
-        days = <div class="day prev-date"> ${prevDays - x + 1}</div>;
+        days = `<div class="day prev-date"> ${prevDays - x + 1}</div>`;
     }
 
     
@@ -56,20 +56,20 @@ function initCalendar() {  //para obtener los días del mes anterior y el mes ac
         if(
         i === new Date().getDate() &&
         year === new Date().getFullYear() &&
-        month === new Date().getfullMonth()
-    ){
-        days += <div class= "day today" > ${i} </div>;
+        month === new Date().getMonth()
+    ) {
+        days += `<div class= "day today" > ${i} </div>;`
     }
     //agregar el reto tal como esta
     else{
-        days += <div class="day"> ${i} </div>;
+        days += `<div class="day"> ${i} </div>`;
     }
     }
     //dia del proximo mes
-        for(let j = 1; j < nextDays; j++){
-            days += <div class="day next-date">${j}</div>;
+        for(let j = 1; j <= nextDays; j++){
+            days += `<div class="day next-date">${j}</div>`;
         }
-        daysContainer.innerHTML = days;
+    daysContainer.innerHTML = days;
 }
 
 initCalendar();
@@ -96,17 +96,52 @@ function nextMonth(){
 //agregar lista de eventos anterior y posterior
 
 prev.addEventListener("click", prevMonth);
-prev.addEventListener("click", nextMonth);
+next.addEventListener("click", nextMonth);
 
 //nuestro calendario está listo, agreguemos la funcionalidad ir a fecha y ir a hoy
 
-todayBtn.addEventListener("click",() =>{
+todayBtn.addEventListener("click",() => {
     today = new Date();
     month = today.getMonth();
     year = today.getFullYear();
     initCalendar();
 });
 
-dateInput.addEventListener("keyup",(e) =>{
-    dateInput.value
-} )
+dateInput.addEventListener("input",(e) => {
+    //permitir solo números eliminar nada más
+    dateInput.value = dateInput.value.replace(/[^0-9]/g, "");
+    if (dateInput.value.lenght === 2){
+        //agregue barra diagonal si ingresan demasiados números
+        dateInput.value += "/";
+    }
+
+    if (dateInput.value.lenght > 7) {
+
+    // no permitir mas de 7 caracteres
+    dateInput.value = dateInput.value.slice(0, 7);
+    }
+    //si eliminamos hasta que la barra no se elimine
+
+    if (e.inputType === "deleteContentBackward") {
+        if (dateInput.value.lenght === 3){
+            dateInput.value = dateInput.value.slice(0, 2);
+        }
+    }
+} );
+
+gotoBtn.addEventListener("click", gotoDate);
+//función para ir a la fecha ingresada
+function gotoDate() {
+    const dateArr = dateInput.value.split("/");
+  //validar alguna fecha  
+    if(dateArr.lenght === 2){
+        if(dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].lenght === 4){
+            month === dateArr[0] -1;
+            year = dateArr[1];
+            initCalendar();
+            return;
+        }
+
+    }
+    alert("invalid date");
+}

8134bf9fb573813724066eef2367be0d23abb371
Ludmila <ludmila.mignone@hotmail.com>
2024-09-27 20:03:40 -0300

    agregamos eventos

--- script.js
+++ script.js
@@ -26,6 +26,39 @@ const months = [
     "Noviembre",
     "Diciembre",
 ];
+//EVENTOS PREDETERMINADOS
+const eventsArr = [
+    {
+        day: 27,
+        month: 11,
+        year:2024,
+        events: [
+            {
+                title: "Event 1 lorem ipsum dolar sit genfa",
+                Time: "10:00 AM",
+            },
+            {
+                title:"Event 2",
+                Time:"12:00 PM",
+            },
+        ],
+    },
+    {
+        day: 18,
+        month: 11,
+        year: 2024,
+        events: [
+            {
+                title: "Event 1 lorem ipsum dolar sit genfa",
+                Time: "10:00 AM",
+            },
+            {
+                title: "Event 2",
+                Time: "12:00 PM",
+            },
+        ],
+    },
+];
 
 // Agregamos la funcion para dias
 function initCalendar() {  //para obtener los días del mes anterior y el mes actual todos los días y los días del mes siguiente
@@ -52,18 +85,37 @@ function initCalendar() {  //para obtener los días del mes anterior y el mes ac
     
     //dia del mes actual
     for ( let i = 1; i <= lastDate; i++) {
-        //si el día es hoy agrega clase hoy
-        if(
+        //COMPROBAR SI evento presente en el día actual
+let event = false
+eventsArr.forEach((eventObj)=>{
+    if(eventObj.day === i && eventObj.month === month && eventObj.year === year){
+        event = true;
+    }
+})
+ 
+    //si el día es hoy agrega clase hoy
+    if(
         i === new Date().getDate() &&
         year === new Date().getFullYear() &&
         month === new Date().getMonth()
     ) {
-        days += `<div class= "day today" > ${i} </div>;`
+
+        // si se encuentra el evento, agregue también la clase de evento
+        // agregar activo hoy al inicio
+        if (event) {
+        days += `<div class= "day today  active event" > ${i} </div>`;
+     } else{
+        days += `<div class= "day today active" > ${i} </div>`;
+     }
     }
     //agregar el resto tal como esta
     else{
-        days += `<div class="day"> ${i} </div>`;
-    }
+        if (event){
+        days += `<div class="day event"> ${i} </div>`;
+        } else{
+            days += `<div class="day"> ${i} </div>`;
+        }
+      }
     }
     //dia del proximo mes
         for(let j = 1; j <= nextDays; j++){
@@ -146,5 +198,123 @@ function gotoDate() {
     alert("invalid date");
 }
 const addEventBtn = document.querySelector(".add-event"),
-  addEvenConteiner = document.querySelector(".add-event-wrapper"),
-  addEventCloseBtn = document.querySelector(".close");
+  addEventContainer = document.querySelector(".add-event-wrapper"),
+  addEventCloseBtn = document.querySelector(".close"),
+  addEventTitle = document.querySelector(".event-name"),
+  addEventFrom = document.querySelector(".event-time-from"),
+  addEventTo = document.querySelector(".event-time-to");
+
+  addEventBtn.addEventListener("click", ()=> {
+    addEventContainer.classList.toggle("active");
+  });
+addEventCloseBtn.addEventLisetner("click", () => {
+    addEventContainer.classList.remove("active");
+});
+document.addEventListener("click", (e) => {
+    //se hace click fuera de lado
+    if (e.target == addEventBtn && !addEventContainer.contains(e.target)){
+        addEventContainer.classList.remove("active");
+    }
+});
+//ponemos solo 50 carateres en el titulo
+addEventTitle.addEventListner("input", (e) =>{
+    addEventTitle.value = addEventTitle.value.slice(0, 50);
+})
+
+//formato de hora en desde y hasta la hora
+addEventFrom.addEventListner("input", (e)=>{
+    //removercualquier numero
+    addEventFrom.value = addEventFrom.value.replace([0 - 9]/g, "");
+    //si se ingresan dos números, se agrega automáticamente
+    if(addEventFrom.value.lenght == 2){
+        addEventFrom.value += ":";
+    }
+    //No dejes que el usuario ingrese más de 5 caracteres.
+    if (addEventFrom.value.lenght > 5) {
+        addEventFrom.value = addEventFrom.value.slice(0,5);
+    }
+});
+// lo mismo con to time
+addEventTo.addEventListener("input", (e) => {
+    //removercualquier numero
+    addEventTo.value = addEventTo.value.replace([0 - 9] / g, "");
+    //si se ingresan dos números, se agrega automáticamente
+    if (addEventTo.value.lenght == 2) {
+        addEventTo.value += ":";
+    }
+    //No dejes que el usuario ingrese más de 5 caracteres.
+    if (addEventTo.value.lenght > 5) {
+        addEventTo.value = addEventTo.value.slice(0, 5);
+    }
+});
+//vamos a crear una función para agregar listner en los días posteriores a la renderización
+function addLister(){
+    const days = document.querySelectorAll(".day");
+    days.forEach((day) => {
+        day.addEventListener("click", (e) => {
+            activeDay = Number(e.target.innerHTML);
+            //llamada activa día después del clic
+            getActiveDay(e.target.innerHTML);
+
+            days.forEach((day) => {
+                day.classList.remove("active");
+            });
+
+            if(e.target.classList.contains("prev-date")) {
+                prevMonth();
+
+                setTimeout(() => {
+                    const days = document.querySelectorAll(".day");
+
+                    days.forEach((day) => {
+                        if (
+                            !day.classList.contains("prev-date") &&
+                            day.innerHTML == e.target.innerHTML
+                        ) {
+                           day.classList.add("active");
+                        }
+                    });
+                }, 100);
+            } else if (e.target.classList.contains(next-date)) {
+                nextMonth();
+
+                setTimeout(() => {
+                    const days = document.querySelectorAll(".day");
+                    days.forEach((day) => {
+                        if (
+                            !day.classList.contains("next-date") && 
+                            day.innerHTML == e.target.innerHTML
+                        ) {
+                            day.classList.add("active");
+                        }
+                    });
+                }, 100);
+            } else {
+                //días restantes del mes recurrente
+                e.target.classList.add("active");
+            }
+        });
+    });
+}
+//permite mostrar los eventos del día activo y agregar la fecha en la parte superior
+
+const eventDay =document.querySelector(".event-day"),
+  eventDate = document.querySelector(".event-date");
+
+function getActiveDay(date) {
+    const day = new Date(year, month, date);
+    const dayName = day.toString().split(" ")[0];
+    eventDay.innerHTML = dayName;
+    eventDate.innerHTML = date + " " + months[month] + " " + year;
+}
+
+
+
+
+
+
+
+
+
+
+
